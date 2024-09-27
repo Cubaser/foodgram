769880ec99aa76251e83c3e77bbaf33ab92231ca
Ivanov Viktor <cubaser@mail.ru>
2024-09-27 10:59:49 +0500

    init

---
+++ backend/foodgram/constants.py
@@ -0,0 +1,3 @@
+class Constants():
+    COOKING_TIME_MIN_VALUE = 1
+    COOKING_TIME_MIN_MASSAGE = 'Слишком низкое значение'
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,15 +1,48 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
+from foodgram.constants import Constants
 
-User= get_user_model()
+User = get_user_model()
+
 
 class Tag(models.Model):
-    title = ...
-    slug = ...
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+        unique=True
+    )
+    slug = models.SlugField(
+        max_length=256,
+        verbose_name='Слаг',
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
 
 class Ingredient(models.Model):
-    title = ...
-    unit = ...
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+        unique=True
+    )
+    measurement_unit = models.CharField(
+        verbose_name='Единицы измерения',
+        max_length=10 # надо уточнить
+    )
+
+    class Meta:
+        verbose_name = 'ингридиент'
+        verbose_name_plural = 'Ингридиенты'
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
@@ -17,6 +50,7 @@ class Recipe(models.Model):
         User,
         verbose_name='Автор',
         on_delete=models.CASCADE,
+        related_name='recipe'
     )
     name = models.CharField(
         max_length=256,
@@ -29,8 +63,29 @@ class Recipe(models.Model):
     text = models.TextField(
         verbose_name='Описание'
     )
-    ingredients = models.ForeignKey(
-
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        verbose_name='Ингридиенты',
+        related_name='recipe'
     )
-    tags = ...
-    cooking_time = ...
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Теги',
+        related_name='recipe'
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        verbose_name='Время приготовления',
+        validators=[
+            MinValueValidator(
+                Constants.COOKING_TIME_MIN_VALUE,
+                Constants.COOKING_TIME_MIN_MASSAGE
+            )
+        ]
+    )
+
+    class Meta:
+        verbose_name = 'рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
