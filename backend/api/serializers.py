d3a79520aab84e639a0f9da08f495659842d37fb
Ivanov Viktor <cubaser@mail.ru>
2024-10-24 00:08:54 +0500

    first_review

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
+
 from foodgram.constants import Constants
-from recipes.models import Favorite, Ingredient, Recipe, RecipeIngredient, Tag
-from user.models import Subscription, User
+from recipes.models import Ingredient, Recipe, RecipeIngredient, Tag
+from user.models import User
 
 
 class TagSerializer(serializers.ModelSerializer):
